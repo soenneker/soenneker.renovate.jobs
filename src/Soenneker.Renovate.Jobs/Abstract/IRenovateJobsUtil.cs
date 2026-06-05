e3f8c2ad8316a0ac462c288008b1627a01e5075c
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:37:24 -0500

    xml commenting

--- src/Soenneker.Renovate.Jobs/Abstract/IRenovateJobsUtil.cs
+++ src/Soenneker.Renovate.Jobs/Abstract/IRenovateJobsUtil.cs
@@ -8,5 +8,13 @@ namespace Soenneker.Renovate.Jobs.Abstract;
 /// </summary>
 public interface IRenovateJobsUtil
 {
+    /// <summary>
+    /// Adds job.
+    /// </summary>
+    /// <param name="username">The username.</param>
+    /// <param name="repository">The repository.</param>
+    /// <param name="sessionCookie">The session cookie.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<string?> AddJob(string username, string repository, string sessionCookie, CancellationToken cancellationToken = default);
 }
